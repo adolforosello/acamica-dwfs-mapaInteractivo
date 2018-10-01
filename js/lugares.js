a34697d6434557099ed9b08a83f66c2b5ed403ff
Adolfo <adolforosello@gmail.com>
2018-10-01 19:35:39 -0300

    guia parte 1/3 completa

---
+++ js/lugares.js
@@ -0,0 +1,46 @@
+lugaresModulo = (function () {
+  var servicioLugares // Servicio para obtener lugares cercanos e información de lugares(como fotos, puntuación del lugar,etc).
+
+    // Completa las direcciones ingresadas por el usuario a y establece los límites
+    // con un círculo cuyo radio es de 20000 metros.
+  function autocompletar () {
+        /* Completar la función autocompletar(): autocompleta los 4 campos de texto de la
+        página (las direcciones ingresables por el usuario).
+        Para esto creá un círculo con radio de 20000 metros y usalo para fijar
+        los límites de la búsqueda de dirección. El círculo no se debe ver en el mapa. */
+  }
+
+    // Inicializo la variable servicioLugares y llamo a la función autocompletar
+  function inicializar () {
+    servicioLugares = new google.maps.places.PlacesService(mapa)
+    autocompletar()
+  }
+
+    // Busca lugares con el tipo especificado en el campo de TipoDeLugar
+
+  function buscarCerca (posicion) {
+    var radio=document.getElementById('radio').value;
+    var lugares=document.getElementById('tipoDeLugar').value;
+    var request={
+      location:posicion,
+      radius:radio,
+      type:lugares
+    };
+      servicio = new google.maps.places.PlacesService(mapa);
+      servicio.nearbySearch(request,function(result,status){
+        console.log(request);
+        console.log(result);
+        console.log(status);
+        
+        marcadorModulo.marcarLugares(result,status);
+      });
+        /* Completar la función buscarCerca  que realice la búsqueda de los lugares
+    del tipo (tipodeLugar) y con el radio indicados en el HTML cerca del lugar
+    pasado como parámetro y llame a la función marcarLugares. */
+
+  }
+  return {
+    inicializar,
+    buscarCerca
+  }
+})()
